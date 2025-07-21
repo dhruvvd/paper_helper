9855d43683535de9fb7da20a247dfeb348967bfd
dhruv <dhruv.dhananjay7@gmail.com>
2025-07-21 12:41:16 -0400

    added funcitionality to hide the popover when i click on the cancel button

--- content.js
+++ content.js
@@ -1,9 +1,13 @@
-console.log("its running");
 const paperBtnClick = document.getElementById('papers-button');
-console.log("btnclick " + paperBtnClick);
 const repaperPage = document.getElementById('id-paper-container');
-console.log("repaperpage " + repaperPage);
 
 paperBtnClick.addEventListener('click', () => {
     repaperPage.style.display = 'flex';
+});
+
+const cancelBtnClick = document.getElementById('cancel-button');
+const paperPopover = document.getElementById('id-pcc');
+
+cancelBtnClick.addEventListener('click', () => {
+    paperPopover.hidePopover();
 });
